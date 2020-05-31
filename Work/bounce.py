5e885bd43efb552f7a56c5666d00a976b9b57329
ShaoshiZ <0zhangshaoshi0@gmail.com>
2020-05-31 22:30:49 +0800

    Add exercise solution 1.5 & 1.6

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+initial_height = 100
+
+i = 0
+while i < 10:
+    i = i+1
+    height = round(initial_height * 3/5, 4)
+    initial_height = height
+    print(height)
